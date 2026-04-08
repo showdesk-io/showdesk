22b4a302ca88186bab1e7f1d1cbc4d002a695340
Dimitri Refauvelet <dimitri.refauvelet@gmail.com>
2026-04-08 11:01:26 +0200

    feat(Settings): add EMAIL_USE_SSL in base settings

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -29,7 +29,6 @@ THIRD_PARTY_APPS = [
     "django_extensions",
     "storages",
     "channels",
-    "anymail",
 ]
 
 LOCAL_APPS = [
@@ -258,7 +257,8 @@ DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="noreply@showdesk.io")
 # SMTP settings (when using django.core.mail.backends.smtp.EmailBackend)
 EMAIL_HOST = config("EMAIL_HOST", default="smtp.example.com")
 EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=False, cast=bool)
+EMAIL_USE_SSL = config("EMAIL_USE_SSL", default=True, cast=bool)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
 
